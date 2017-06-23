5f013fbd4c9ca0ca3bd2702673c045368f2e6cc1
Adam Feuer <adam@adamfeuer.com>
2017-06-23 14:46:32 -0700

    update publish.sh

--- utils/publish.sh
+++ utils/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -x
-LOCAL_THEME_PATH=$(cd ../$( dirname `which $0` ); pwd )
-REMOTE_THEME_PATH="/var/www-adamfeuer.com/wp-content/themes/starcat-foundationpress"
-echo "syncing $THEME_PATH to starcat.io..."
+LOCAL_THEME_PATH=$(cd $( dirname $( dirname `which $0` ) ) ; pwd )
+REMOTE_THEME_PATH="/var/www-adamfeuer.com/wp-content/themes"
+echo "syncing $LOCAL_THEME_PATH to starcat.io..."
 #rsync -av --no-owner --no-perms --no-times --exclude "utils" --exclude ".git" --exclude "node_modules" $LOCAL_THEME_PATH adamfeuer.com:$REMOTE_THEME_PATH
 rsync -av --no-owner --exclude "utils" --exclude ".git" --exclude "node_modules" $LOCAL_THEME_PATH adamfeuer.com:$REMOTE_THEME_PATH
 ssh adamfeuer.com "sudo chgrp -R www-data $REMOTE_THEME_PATH"
